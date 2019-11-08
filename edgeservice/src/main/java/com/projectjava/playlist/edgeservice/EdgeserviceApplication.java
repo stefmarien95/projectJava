508c282d916dc8b3b06509d1538c873bbdc60f2a
ArneVDE <r0704140@student.thomasmore.be>
2019-11-08 11:31:51 +0100

    Edgeservice

---
+++ edgeservice/src/main/java/com/projectjava/playlist/edgeservice/EdgeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.projectjava.playlist.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeserviceApplication.class, args);
+	}
+
+}
